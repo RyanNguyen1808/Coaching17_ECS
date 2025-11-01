b8f8dfee87b6be5cbf80619846420986b8889bd6
NyahDC <131533317+NyahDC@users.noreply.github.com>
2025-11-01 12:37:46 +0800

    add task definition

---
+++ ecs.tf
@@ -0,0 +1,36 @@
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.9.0"
+
+  cluster_name = "${local.prefix}-ecs"
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+      }
+    }
+  }
+
+  services = {
+    Group4-taskdefition = { #task definition and service name -> #Change
+      cpu    = 512
+      memory = 1024
+      container_definitions = {
+        Group4-NAME = { #container name -> Change
+          essential = true
+          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
+          port_mappings = [
+            {
+              containerPort = 8080
+              protocol      = "tcp"
+            }
+          ]
+        }
+      }
+      assign_public_ip                   = true
+      deployment_minimum_healthy_percent = 100
+      subnet_ids                   = [] #List of subnet IDs to use for your tasks
+      security_group_ids           = [] #Create a SG resource and pass it here
+    }
+  }
+}
